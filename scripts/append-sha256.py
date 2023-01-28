3ae40fb38940e1182cbe6613e91dfbaef0a02d6b
Vladislav <p1nchp0d@bk.ru>
2023-01-28 12:19:33 +0300

    get a list of symlinks to be excluded

--- scripts/append-sha256.py
+++ scripts/append-sha256.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python3
 import sys, hashlib, os
 pfiles = sys.argv[1]+'/proprietary-files.txt'
+mkfile = sys.argv[1]+'/Android.mk'
 if not os.path.exists(pfiles):
     print(pfiles, 'doesnt exist\nmake sure you have followed the instructions below\nhttps://grapheneos.org/build#extracting-vendor-files-for-pixel-devices')
     exit()
-lines = []
+plines = []
+mklines = []
 shalines = []
+with open(mkfile, 'r') as f:
+    for line in f:
+        if line.startswith("    ln -sf"):
+            line = line.replace(";", "")
+            line = line.replace("\\", "")
+            line = line.replace("$(PRODUCT_OUT)/", "")
+            arrline = line.split()
+            mklines.append(arrline[3]) 
 with open(pfiles, 'r') as f:
     for line in f:
         line = line.partition('#')[0].rstrip()
-        # Drop broken lib
-        if line == "system_ext/app/com.qualcomm.qti.services.secureui/lib/arm64/libsecureuisvc_jni.so":
-            line = ''
-        lines.append(line)
-for i in range(len(lines)):
-    if lines[i] != '':
-        blob = sys.argv[1]+'/proprietary/'+lines[i]
+        if line in mklines:
+            continue
+        plines.append(line)
+for i in range(len(plines)):
+    if plines[i] != '':
+        blob = sys.argv[1]+'/proprietary/'+plines[i]
         if "PRESIGNED" in blob:
             blob = blob.replace(";PRESIGNED", "")
         with open(blob, "rb") as f:
             bytes = f.read()
             rhash = hashlib.sha256(bytes).hexdigest()
-        shalines.append(lines[i]+":SHA256:"+rhash)
+        shalines.append(plines[i]+":SHA256:"+rhash)
         
 with open(pfiles+'.sha256', 'w') as f:
     for line in shalines:
